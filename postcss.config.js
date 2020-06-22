e91b5229f71697ac95704e53d6ad25a9ec0b6248
rickydamarsaputra <retrocode.rc@gmail.com>
2020-06-22 07:12:36 +0700

    deploy to github

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+module.exports = {
+	plugins: [
+		require("tailwindcss"),
+		// purgecss({
+		// 	content: ["./src/index.html"],
+		// }),
+	],
+};
